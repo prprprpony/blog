62e20b60bc5b4a97c68584ba751a1c71ca48b40f
prpr <3254tg89@gmail.com>
2016-06-24 00:55:54 +0800

    bug fix: no background in posts

--- assets/js/random-background-image.js
+++ assets/js/random-background-image.js
@@ -1,7 +1,9 @@
-(function ($) {
+(function () {
     var num_bg = 6;
     var n = Math.floor(Math.random() * num_bg).toString();
-    ["#header", "#footer"].forEach(function (s) {
-        $(s).css("background-image", 'url(images/overlay.png), url(images/backgrounds/bg' + n + '.svg)');
+    var base_url = window.location.origin + '/blog/';
+    var img_url = 'url(' + base_url + 'assets/css/images/overlay.png), url(' + base_url + 'images/backgrounds/bg' + n + '.svg)';
+    ["header", "footer"].forEach(function (s) {
+        document.getElementById(s).style.backgroundImage = img_url;
     });
-})(jQuery);
+})();
